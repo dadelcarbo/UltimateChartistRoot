dd06d4d7d53b3d8bb0c0a90634839d750eda4242
dadelcarbo <david.carbonel@volvo.com>
2018-09-11 08:07:09 +0200

    UPdate watchlist

--- CleanBarChart.bat
+++ CleanBarChart.bat
@@ -1,3 +1,5 @@
-RMDIR /Q /S data\daily\BarChart
-RMDIR /Q /S data\archive\daily\BarChart
+RMDIR /Q /S data\daily\BarChart2
+MOVE data\daily\BarChart data\daily\BarChart2
+RMDIR /Q /S data\archive\daily\BarChart2
+MOVE data\archive\daily\BarChart data\archive\daily\BarChart2
 @PAUSE
